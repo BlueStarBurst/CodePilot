6da2bdfdeaa96182a9c0968a6fd6ac1553dd6a07
bluestarburst <hargreaves.bryant@gmail.com>
2023-10-23 15:29:15 -0500

    first commit

---
+++ src/app.js
@@ -0,0 +1,16 @@
+import React, {
+	useRef,
+	useState,
+	useEffect,
+	useCallback,
+	useLayoutEffect,
+	Suspense,
+} from "react";
+import { render } from "react-dom";
+
+import "./style.scss";
+import "bootstrap/dist/css/bootstrap.min.css";
+
+
+
+render(<h1>Hi</h1>, document.getElementById("root"));
